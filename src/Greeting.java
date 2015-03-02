d344d45c1d39fc72757de197c8a2f0ab20931ca3
unknown <yirawan@LectCR2-1.iss.nus.edu.sg>
2015-03-02 15:26:51 +0800

    Implemented greeting based on user name

--- src/Greeting.java
+++ src/Greeting.java
@@ -1,19 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Calendar;
 
 
 public class Greeting {
 
 	public static void main(String[] args) {
+		
+		System.out.println("Please enter your name:");
+
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		String name="";
+		try {name=bf.readLine();} catch (IOException ex) {};
+
+
+		
 		Calendar cal = Calendar.getInstance();
 		
 		int hour = cal.get(Calendar.HOUR_OF_DAY);
 		
 		if (hour <12) {
-			System.out.println("good day!");
+			System.out.println("good day "+ name + "!");
 		} else if (hour <19) {
-			System.out.println("good afternoon!");
+			System.out.println("good afternoon "+ name + "!");
 		} else {
-			System.out.println("good evening!");			
+			System.out.println("good evening "+ name + "!");			
 		}
 	}
 
